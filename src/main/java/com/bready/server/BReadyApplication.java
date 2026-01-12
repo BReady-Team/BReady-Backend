5c108d2827611ae8b8e714d7d19f95bd5bdb64c9
seungin <tmddls91@inha.edu>
2026-01-12 22:51:26 +0900

    [init] 프로젝트 기초 세팅

---
+++ src/main/java/com/bready/server/BReadyApplication.java
@@ -0,0 +1,13 @@
+package com.bready.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BReadyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BReadyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bready/server/BReadyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bready.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BReadyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
