610e23b1e36500508efb82d8e1a0609bca20acdc
seungin <tmddls91@inha.edu>
2026-01-16 21:30:10 +0900

    [feat] actuator 허용 관련 세분화

--- src/main/java/com/bready/server/global/config/security/SecurityConfig.java
+++ src/main/java/com/bready/server/global/config/security/SecurityConfig.java
@@ -46,7 +46,7 @@ public class SecurityConfig {
                                 "/swagger-ui.html",
                                 "/v3/api-docs/**"
                         ).permitAll()
-                        .requestMatchers("/actuator/**").permitAll()
+                        .requestMatchers("/actuator/health", "/actuator/info").permitAll()
                         .requestMatchers("/api/v1/auth/**").permitAll()
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         // TODO: 로그인,회원가입 API 구현 후 .anyRequest().authenticated() 로 변경
