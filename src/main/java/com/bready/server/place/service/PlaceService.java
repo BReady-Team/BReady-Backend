261237a7dc0a72358c7407377872ebea4bd2a4b9
seungin <tmddls91@inha.edu>
2026-01-18 01:02:05 +0900

    [feat] Place 도메인 레포지토리,컨트롤러,서비스 로직 초기 구성

---
+++ src/main/java/com/bready/server/place/service/PlaceService.java
@@ -0,0 +1,9 @@
+package com.bready.server.place.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PlaceService {
+}
