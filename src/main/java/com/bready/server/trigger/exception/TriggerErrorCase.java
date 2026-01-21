4ad86e0effe0e9a240337fa6b7d4c191203ade43
seungin <tmddls91@inha.edu>
2026-01-21 21:17:14 +0900

    [feat] 통계,트리거 도메인 예외처리 관련 기반 코드 추가

---
+++ src/main/java/com/bready/server/trigger/exception/TriggerErrorCase.java
@@ -0,0 +1,32 @@
+package com.bready.server.trigger.exception;
+
+import com.bready.server.global.exception.ErrorCase;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum TriggerErrorCase implements ErrorCase {
+
+    TRIGGER_NOT_FOUND(HttpStatus.NOT_FOUND, 7001, "트리거를 찾을 수 없습니다."),
+    TRIGGER_ALREADY_EXECUTED(HttpStatus.BAD_REQUEST, 7002, "이미 실행된 트리거입니다.");
+    private final HttpStatus httpStatus;
+    private final Integer errorCode;
+    private final String message;
+
+    @Override
+    public Integer getHttpStatusCode() {
+        return httpStatus.value();
+    }
+
+    @Override
+    public Integer getErrorCode() {
+        return errorCode;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+}
