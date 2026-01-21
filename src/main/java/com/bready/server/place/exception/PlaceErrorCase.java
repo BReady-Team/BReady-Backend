043835e4eeb4ee5dee94ce493d381d375cfdc44c
seungin <tmddls91@inha.edu>
2026-01-21 22:38:02 +0900

    [feat] 카카오 검색 관련 ErrorCase 추가

--- src/main/java/com/bready/server/place/exception/PlaceErrorCase.java
+++ src/main/java/com/bready/server/place/exception/PlaceErrorCase.java
@@ -10,8 +10,11 @@ import org.springframework.http.HttpStatus;
 public enum PlaceErrorCase implements ErrorCase {
 
     PLACE_SEARCH_FAILED(HttpStatus.BAD_REQUEST, 4001, "장소 검색에 실패했습니다."),
-    INVALID_PLACE_TYPE(HttpStatus.BAD_REQUEST, 3002, "장소 타입이 유효하지 않습니다."),
-    PLACE_ACCESS_DENIED(HttpStatus.FORBIDDEN, 3003, "해당 장소에 대한 접근 권한이 없습니다.");
+    INVALID_PLACE_TYPE(HttpStatus.BAD_REQUEST, 4002, "장소 타입이 유효하지 않습니다."),
+    PLACE_ACCESS_DENIED(HttpStatus.FORBIDDEN, 4003, "해당 장소에 대한 접근 권한이 없습니다."),
+    INVALID_SEARCH_KEYWORD(HttpStatus.BAD_REQUEST, 4100, "검색 키워드가 비어있습니다."),
+    KAKAO_CLIENT_ERROR(HttpStatus.BAD_GATEWAY, 4101, "카카오 장소 검색 요청이 잘못되었습니다."),
+    KAKAO_SERVER_ERROR(HttpStatus.BAD_GATEWAY, 4102, "카카오 장소 서비스에 장애가 발생했습니다.");
 
     private final HttpStatus httpStatus;
     private final Integer errorCode;
