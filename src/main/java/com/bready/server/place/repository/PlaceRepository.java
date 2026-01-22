03d5e842853f805ac4e8050fe60dfad4877055f2
seungin <tmddls91@inha.edu>
2026-01-23 03:24:29 +0900

    [feat] 장소 후보 관련 레포지토리 구성

--- src/main/java/com/bready/server/place/repository/PlaceRepository.java
+++ src/main/java/com/bready/server/place/repository/PlaceRepository.java
@@ -3,5 +3,8 @@ package com.bready.server.place.repository;
 import com.bready.server.place.domain.Place;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PlaceRepository extends JpaRepository<Place, Long> {
+    Optional<Place> findByExternalId(String externalId);
 }
