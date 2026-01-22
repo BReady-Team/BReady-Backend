adf5ec073bf58b2ad12553f890622b99dc07adae
seungin <tmddls91@inha.edu>
2026-01-23 06:21:55 +0900

    [refactor] 대표 장소 선택 관련 엔티티 및 레포지토리 보완

--- src/main/java/com/bready/server/plan/repository/CategoryStateRepository.java
+++ src/main/java/com/bready/server/plan/repository/CategoryStateRepository.java
@@ -3,5 +3,8 @@ package com.bready.server.plan.repository;
 import com.bready.server.plan.domain.CategoryState;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CategoryStateRepository extends JpaRepository<CategoryState, Long> {
+    Optional<CategoryState> findByCategory_Id(Long categoryId);
 }
