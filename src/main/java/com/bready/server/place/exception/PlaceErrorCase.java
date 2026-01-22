5eb132f1de3093c397190b13ca6e13bbe576ddfc
seungin <tmddls91@inha.edu>
2026-01-23 03:12:27 +0900

    [refactor] 장소 도메인 ErrorCase 추가

--- src/main/java/com/bready/server/place/exception/PlaceErrorCase.java
+++ src/main/java/com/bready/server/place/exception/PlaceErrorCase.java
@@ -14,7 +14,10 @@ public enum PlaceErrorCase implements ErrorCase {
     PLACE_NOT_FOUND(HttpStatus.NOT_FOUND, 4102, "조건에 맞는 장소를 찾을 수 없습니다."),
     KAKAO_CLIENT_ERROR(HttpStatus.BAD_GATEWAY, 4103, "카카오 장소 검색 요청이 잘못되었습니다."),
     KAKAO_SERVER_ERROR(HttpStatus.BAD_GATEWAY, 4104, "카카오 장소 서비스에 장애가 발생했습니다."),
-    KAKAO_RESPONSE_PARSE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, 4105, "카카오 장소 응답 파싱에 실패했습니다.");
+    KAKAO_RESPONSE_PARSE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, 4105, "카카오 장소 응답 파싱에 실패했습니다."),
+    DUPLICATE_PLACE_CANDIDATE(HttpStatus.CONFLICT, 4106, "이미 해당 카테고리에 등록된 장소입니다."),
+    INVALID_PLACE_OR_CATEGORY(HttpStatus.BAD_REQUEST, 4107, "유효하지 않은 플랜 또는 카테고리입니다.");
+
 
     private final HttpStatus httpStatus;
     private final Integer errorCode;
