56c58da6683ed9022268e72ca310ba1ab62a96fc
axoesnxm <sula9139@gmail.com>
2026-01-23 07:27:09 +0900

    [feat] 회원가입 API 컨 컨트롤러 추가

--- src/main/java/com/bready/server/auth/controller/AuthController.java
+++ src/main/java/com/bready/server/auth/controller/AuthController.java
@@ -1,11 +1,22 @@
 package com.bready.server.auth.controller;
 
+import com.bready.server.auth.dto.SignupRequest;
+import com.bready.server.auth.dto.SignupResponse;
+import com.bready.server.auth.service.AuthService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/signup")
+    @ResponseStatus(HttpStatus.CREATED)
+    public SignupResponse signup(@RequestBody SignupRequest request) {
+        return authService.signup(request);
+    }
 }
