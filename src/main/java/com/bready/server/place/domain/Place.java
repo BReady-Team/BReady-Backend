2dae829832091f838f4c9a9f8b9d5c032114c807
seungin <tmddls91@inha.edu>
2026-01-23 03:18:02 +0900

    [refactor] 장소 엔티티 정적 팩토리 메서드 적용

--- src/main/java/com/bready/server/place/domain/Place.java
+++ src/main/java/com/bready/server/place/domain/Place.java
@@ -2,15 +2,16 @@ package com.bready.server.place.domain;
 
 import com.bready.server.global.entity.BaseEntity;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
-@Getter
-@NoArgsConstructor
 @Entity
 @Table(name = "places")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Place extends BaseEntity {
 
     @Id
@@ -18,7 +19,7 @@ public class Place extends BaseEntity {
     private Long id;
 
     // 외부 API 장소 ID (카카오, 네이버 등)
-    @Column(name = "external_id")
+    @Column(name = "external_id", nullable = false, unique = true)
     private String externalId;
 
     @Column(nullable = false)
@@ -34,4 +35,22 @@ public class Place extends BaseEntity {
 
     @Column(name = "is_indoor")
     private Boolean isIndoor;
+
+    public static Place create(
+            String externalId,
+            String name,
+            String address,
+            BigDecimal latitude,
+            BigDecimal longitude,
+            Boolean isIndoor
+    ) {
+        Place place = new Place();
+        place.externalId = externalId;
+        place.name = name;
+        place.address = address;
+        place.latitude = latitude;
+        place.longitude = longitude;
+        place.isIndoor = isIndoor;
+        return place;
+    }
 }
