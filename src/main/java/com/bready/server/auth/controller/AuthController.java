a5b253ebf17248e8da843a80fa1a1fbf06c81fa1
axoesnxm <sula9139@gmail.com>
2026-01-23 08:10:53 +0900

    [feat] 회원가입 컨트롤러에 @Valid 추가

--- src/main/java/com/bready/server/auth/controller/AuthController.java
+++ src/main/java/com/bready/server/auth/controller/AuthController.java
@@ -3,6 +3,7 @@ package com.bready.server.auth.controller;
 import com.bready.server.auth.dto.SignupRequest;
 import com.bready.server.auth.dto.SignupResponse;
 import com.bready.server.auth.service.AuthService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -16,7 +17,7 @@ public class AuthController {
 
     @PostMapping("/signup")
     @ResponseStatus(HttpStatus.CREATED)
-    public SignupResponse signup(@RequestBody SignupRequest request) {
+    public SignupResponse signup(@Valid @RequestBody SignupRequest request) {
         return authService.signup(request);
     }
 }
