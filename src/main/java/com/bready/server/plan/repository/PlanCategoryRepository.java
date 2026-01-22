d91476db809e0d18f3eb2718d9a324f5f6ad4c86
seungin <tmddls91@inha.edu>
2026-01-23 03:33:09 +0900

    [feat] 일정 카테고리 레포지토리 구성

--- src/main/java/com/bready/server/plan/repository/PlanCategoryRepository.java
+++ src/main/java/com/bready/server/plan/repository/PlanCategoryRepository.java
@@ -3,5 +3,9 @@ package com.bready.server.plan.repository;
 import com.bready.server.plan.domain.PlanCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PlanCategoryRepository extends JpaRepository<PlanCategory, Long> {
+    // 장소 후보쪽에서 category가 plan에 속하는지 검증하기 위해서 추가
+    Optional<PlanCategory> findByIdAndPlan_Id(Long categoryId, Long planId);
 }
