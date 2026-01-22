4059f81bf5f7e8dcfa2c525876473fb05b6496f8
seungin <tmddls91@inha.edu>
2026-01-23 06:20:11 +0900

    [feat] 대표 장소 선택 관련 레포지토리 구현 (쿼리 포함)

--- src/main/java/com/bready/server/place/repository/PlaceCandidateRepository.java
+++ src/main/java/com/bready/server/place/repository/PlaceCandidateRepository.java
@@ -2,6 +2,17 @@ package com.bready.server.place.repository;
 
 import com.bready.server.place.domain.PlaceCandidate;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface PlaceCandidateRepository extends JpaRepository<PlaceCandidate, Long> {
+    @Query("""
+        select pc
+        from PlaceCandidate pc
+        join fetch pc.category c
+        join fetch pc.place p
+        where pc.id = :candidateId
+    """)
+    Optional<PlaceCandidate> findByIdWithCategoryAndPlace(Long candidateId);
 }
