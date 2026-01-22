3ff2754535639a8ece13a3e6a3cdc8df7e5c2732
axoesnxm <sula9139@gmail.com>
2026-01-23 05:12:42 +0900

    [feat]  회원가입을 위한 사용자 조회 메서드 추가

--- src/main/java/com/bready/server/user/repository/UserRepository.java
+++ src/main/java/com/bready/server/user/repository/UserRepository.java
@@ -3,5 +3,11 @@ package com.bready.server.user.repository;
 import com.bready.server.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByEmail(String email);
+
+    Optional<User> findByEmail(String email);
 }
