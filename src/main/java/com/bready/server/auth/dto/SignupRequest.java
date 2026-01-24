3d23c3c6b8a03cb40f1d18d14cebbc8b2dc055f0
axoesnxm <sula9139@gmail.com>
2026-01-24 13:44:49 +0900

    [feat] 회원가입 요청 DTO 검증 어노테이션 추가

--- src/main/java/com/bready/server/auth/dto/SignupRequest.java
+++ src/main/java/com/bready/server/auth/dto/SignupRequest.java
@@ -1,10 +1,24 @@
 package com.bready.server.auth.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 
 @Getter
 public class SignupRequest {
+
+    @NotBlank(message = "닉네임을 입력해주세요.")
     private String nickname;
+
+    @NotBlank(message = "이메일을 입력해주세요.")
+    @Email(message = "이메일 형식이 올바르지 않습니다.")
     private String email;
+
+    @NotBlank(message = "비밀번호를 입력해주세요.")
+    @Pattern(
+            regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&]).{8,}$",
+            message = "비밀번호가 정책을 충족하지 않습니다."
+    )
     private String password;
 }
