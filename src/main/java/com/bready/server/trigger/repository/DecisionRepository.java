b41f60ffe818bc9b00d5406e968743a3d246b147
seungin <tmddls91@inha.edu>
2026-01-27 04:20:32 +0900

    [feat] 스위치에서 category/plan/stat까지 로딩하기 위한 레포지토리 구성 추가

--- src/main/java/com/bready/server/trigger/repository/DecisionRepository.java
+++ src/main/java/com/bready/server/trigger/repository/DecisionRepository.java
@@ -2,10 +2,21 @@ package com.bready.server.trigger.repository;
 
 import com.bready.server.trigger.domain.Decision;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface DecisionRepository extends JpaRepository<Decision, Long> {
     boolean existsByTrigger_Id(Long triggerId);
     Optional<Decision> findByTrigger_Id(Long triggerId);
+
+    @Query("""
+        select d
+        from Decision d
+        join fetch d.trigger t
+        join fetch t.category c
+        join fetch t.plan p
+        where d.id = :decisionId
+    """)
+    Optional<Decision> findByIdWithTriggerPlanCategory(Long decisionId);
 }
