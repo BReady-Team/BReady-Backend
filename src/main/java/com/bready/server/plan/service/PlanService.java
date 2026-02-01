a9004cf5f449de459488525dc20826c78199b1c1
axoesnxm <sula9139@gmail.com>
2026-02-01 17:03:33 +0900

    [feat] 플랜 생성 서비스 구현

--- src/main/java/com/bready/server/plan/service/PlanService.java
+++ src/main/java/com/bready/server/plan/service/PlanService.java
@@ -1,9 +1,29 @@
 package com.bready.server.plan.service;
 
+import com.bready.server.plan.domain.Plan;
+import com.bready.server.plan.dto.PlanCreateRequest;
+import com.bready.server.plan.dto.PlanCreateResponse;
+import com.bready.server.plan.repository.PlanRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class PlanService {
+
+    private final PlanRepository planRepository;
+
+    @Transactional
+    public PlanCreateResponse createPlan(Long userId, PlanCreateRequest request) {
+
+        Plan plan = Plan.create(userId, request.getTitle(), request.getPlanDate(), request.getRegion());
+
+        Plan saved = planRepository.save(plan);
+
+        return PlanCreateResponse.builder()
+                .planId(saved.getId())
+                .createdAt(saved.getCreatedAt())
+                .build();
+    }
 }
