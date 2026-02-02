6af823de192384c307aed894a202ad84cd9f07d5
axoesnxm <sula9139@gmail.com>
2026-02-03 04:58:57 +0900

    [feat] 플랜 상세 조회 응답 DTO 추가

---
+++ src/main/java/com/bready/server/plan/dto/PlanDto.java
@@ -0,0 +1,21 @@
+package com.bready.server.plan.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+public class PlanDto {
+
+    private Long planId;
+    private String title;
+    private LocalDate planDate;
+    private String region;
+    private String status;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+}
