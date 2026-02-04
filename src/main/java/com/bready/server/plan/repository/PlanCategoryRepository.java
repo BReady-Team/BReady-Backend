dcd302906d1123f17a541ea18ef42361ca79cb5b
seungin <tmddls91@inha.edu>
2026-02-05 01:59:20 +0900

    [feat] 카테고리 타입을 가져오기 위한 카테고리 관련 쿼리 추가

--- src/main/java/com/bready/server/plan/repository/PlanCategoryRepository.java
+++ src/main/java/com/bready/server/plan/repository/PlanCategoryRepository.java
@@ -2,10 +2,20 @@ package com.bready.server.plan.repository;
 
 import com.bready.server.plan.domain.PlanCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface PlanCategoryRepository extends JpaRepository<PlanCategory, Long> {
+    @Query("""
+    select pc.plan.id, pc.categoryType
+    from PlanCategory pc
+    where pc.plan.ownerId = :ownerId
+    """)
+    List<Object[]> findCategoryTypesByOwner(@Param("ownerId") Long ownerId);
+
     // 장소 후보쪽에서 category가 plan에 속하는지 검증하기 위해서 추가
     Optional<PlanCategory> findByIdAndPlan_Id(Long id, Long planId);
 }
