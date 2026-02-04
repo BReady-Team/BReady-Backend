8a0a1b981d24d7494b6983c6f066f24557d9f13a
seungin <tmddls91@inha.edu>
2026-02-05 00:50:39 +0900

    [feat] 통계 ErrorCase 추가

--- src/main/java/com/bready/server/stats/exception/StatsErrorCase.java
+++ src/main/java/com/bready/server/stats/exception/StatsErrorCase.java
@@ -11,7 +11,9 @@ public enum StatsErrorCase implements ErrorCase {
 
     STATS_NOT_FOUND(HttpStatus.NOT_FOUND, 6001, "통계 데이터를 찾을 수 없습니다."),
     STATS_CALCULATION_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, 6002, "통계 계산에 실패했습니다."),
-    INVALID_PERIOD(HttpStatus.BAD_REQUEST, 6003, "period 값이 올바르지 않습니다.");
+    INVALID_PERIOD(HttpStatus.BAD_REQUEST, 6003, "period 값이 올바르지 않습니다."),
+    INVALID_PARAMETER(HttpStatus.UNAUTHORIZED, 6004, "요청 파라미터가 올바르지 않습니다."),
+    INVALID_LIMIT(HttpStatus.CONFLICT, 6005, "limit 값이 올바르지 않습니다.");
 
     private final HttpStatus httpStatus;
     private final Integer errorCode;
