19c8a91f43361a84df004ad920a6261acec438ba
seungin <tmddls91@inha.edu>
2026-02-05 00:52:02 +0900

    [feat] 플랜 레포지토리 쿼리 추가

--- src/main/java/com/bready/server/plan/repository/PlanRepository.java
+++ src/main/java/com/bready/server/plan/repository/PlanRepository.java
@@ -2,7 +2,24 @@ package com.bready.server.plan.repository;
 
 import com.bready.server.plan.domain.Plan;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface PlanRepository extends JpaRepository<Plan, Long> {
-    long countByOwnerId(Long ownerId);
+    @Query("""
+        select p
+        from Plan p
+        where p.id = :planId
+          and p.ownerId = :ownerId
+    """)
+    Optional<Plan> findByIdAndOwnerId(@Param("planId") Long planId, @Param("ownerId") Long ownerId);
+
+    @Query("""
+        select count(p)
+        from Plan p
+        where p.ownerId = :ownerId
+    """)
+    long countByOwnerId(@Param("ownerId") Long ownerId);
 }
