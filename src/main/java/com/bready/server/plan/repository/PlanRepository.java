81f0a2758432967376dbc2a6b2dd7487a8ab9a79
axoesnxm <sula9139@gmail.com>
2026-02-08 19:32:15 +0900

    [feat] 플랜 목록 조회를 위한 레포지토리 메서드 추가

--- src/main/java/com/bready/server/plan/repository/PlanRepository.java
+++ src/main/java/com/bready/server/plan/repository/PlanRepository.java
@@ -1,7 +1,14 @@
 package com.bready.server.plan.repository;
 
 import com.bready.server.plan.domain.Plan;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+
 public interface PlanRepository extends JpaRepository<Plan, Long> {
+
+    Page<Plan> findAllByOwnerIdAndDeletedAtIsNull(Long ownerId, Pageable pageable);
+
 }
