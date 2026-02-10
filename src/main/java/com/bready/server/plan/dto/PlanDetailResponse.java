cc518431d9780c7b5f9a13a85baad1003d07322f
axoesnxm <sula9139@gmail.com>
2026-02-10 18:32:52 +0900

    [feat] 플랜 상세 조회 응답에 카테고리 목록 필드 추가

--- src/main/java/com/bready/server/plan/dto/PlanDetailResponse.java
+++ src/main/java/com/bready/server/plan/dto/PlanDetailResponse.java
@@ -5,12 +5,14 @@ import lombok.Getter;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @Builder
 public class PlanDetailResponse {
 
     private PlanDto plan;
+    private List<PlanCategoryItemDto> categories;
     // TODO : 장소, 카테고리 등 연결
 
 }
