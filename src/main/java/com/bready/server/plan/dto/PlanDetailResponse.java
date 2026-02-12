a6b9d2dab157a24b62dd9df0063e63d13719126a
axoesnxm <sula9139@gmail.com>
2026-02-12 23:46:27 +0900

    [refactor] 플랜 상세 응답 categories 기본값 빈 리스트로 변경

--- src/main/java/com/bready/server/plan/dto/PlanDetailResponse.java
+++ src/main/java/com/bready/server/plan/dto/PlanDetailResponse.java
@@ -12,7 +12,9 @@ import java.util.List;
 public class PlanDetailResponse {
 
     private PlanDto plan;
-    private List<PlanCategoryItemDto> categories;
-    // TODO : 장소, 카테고리 등 연결
 
+    @Builder.Default
+    private List<PlanCategoryItemDto> categories = List.of();
+
+    // TODO : 장소, 카테고리 등 연결
 }
