3292c3b91e1fa63544690ad41831795ba831d933
seungin <tmddls91@inha.edu>
2026-02-13 00:48:22 +0900

    [refactor] 통계 레포지토리 메서드 추가

--- src/main/java/com/bready/server/stats/repository/PlanStatsRepository.java
+++ src/main/java/com/bready/server/stats/repository/PlanStatsRepository.java
@@ -5,9 +5,15 @@ import com.bready.server.stats.domain.StatsPeriod;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PlanStatsRepository extends JpaRepository<PlanStats, Long> {
     List<PlanStats> findByPlanIdIn(List<Long> planIds);
+
+    Optional<PlanStats> findByPlanIdAndPeriod(
+            Long planId,
+            StatsPeriod period
+    );
 
     List<PlanStats> findByPeriodAndPlanIdIn(
             StatsPeriod period,
