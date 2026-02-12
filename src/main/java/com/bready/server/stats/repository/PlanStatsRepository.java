a1ae5fa41d03b9f0cd0a56dba84858a3cb29edf5
seungin <tmddls91@inha.edu>
2026-02-12 23:44:48 +0900

    [refactor] PlanStats 레포지토리 로직 추가

--- src/main/java/com/bready/server/stats/repository/PlanStatsRepository.java
+++ src/main/java/com/bready/server/stats/repository/PlanStatsRepository.java
@@ -1,7 +1,17 @@
 package com.bready.server.stats.repository;
 
 import com.bready.server.stats.domain.PlanStats;
+import com.bready.server.stats.domain.StatsPeriod;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface PlanStatsRepository extends JpaRepository<PlanStats, Long> {
+    Optional<PlanStats> findByPlanIdAndPeriod(Long planId, StatsPeriod period);
+
+    List<PlanStats> findByPeriodAndPlanIdIn(
+            StatsPeriod period,
+            List<Long> planIds
+    );
 }
