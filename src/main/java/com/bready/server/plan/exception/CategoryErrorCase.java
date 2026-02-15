00363a9a709562e88a3b4bf290ec8abeb0851800
axoesnxm <sula9139@gmail.com>
2026-02-15 15:13:07 +0900

    [fix] CategoryErrorCase 에러 코드 범위 변경 (42XX)

--- src/main/java/com/bready/server/plan/exception/CategoryErrorCase.java
+++ src/main/java/com/bready/server/plan/exception/CategoryErrorCase.java
@@ -9,10 +9,10 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum CategoryErrorCase implements ErrorCase {
 
-    CATEGORY_ACCESS_DENIED(HttpStatus.FORBIDDEN, 4101, "카테고리에 대한 접근 권한이 없습니다."),
-    CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, 4102, "존재하지 않는 카테고리입니다."),
-    INVALID_SEQUENCE(HttpStatus.BAD_REQUEST, 4103, "sequence 값이 올바르지 않습니다."),
-    INVALID_ORDERS(HttpStatus.BAD_REQUEST, 4104, "orders 값이 올바르지 않습니다.");
+    CATEGORY_ACCESS_DENIED(HttpStatus.FORBIDDEN, 4201, "카테고리에 대한 접근 권한이 없습니다."),
+    CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, 4202, "존재하지 않는 카테고리입니다."),
+    INVALID_SEQUENCE(HttpStatus.BAD_REQUEST, 4203, "sequence 값이 올바르지 않습니다."),
+    INVALID_ORDERS(HttpStatus.BAD_REQUEST, 4204, "orders 값이 올바르지 않습니다.");
 
     private final HttpStatus httpStatus;
     private final Integer errorCode;
