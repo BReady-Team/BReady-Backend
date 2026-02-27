40d6fcc9c4e7c0405283eb4c49e625274c214243
axoesnxm <sula9139@gmail.com>
2026-02-28 02:10:37 +0900

    [chore] ChatClient 빈 조건부 등록으로 CI 안전성 확보

--- src/main/java/com/bready/server/global/config/RecommendationAiConfig.java
+++ src/main/java/com/bready/server/global/config/RecommendationAiConfig.java
@@ -2,6 +2,7 @@ package com.bready.server.global.config;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 public class RecommendationAiConfig {
 
     @Bean
+    @ConditionalOnBean(ChatModel.class)
     public ChatClient chatClient(ChatModel chatModel) {
         return ChatClient.builder(chatModel).build();
     }
