74f82b03d43f9e4f0cdfcf5a016712a4782a0cc2
seungin <tmddls91@inha.edu>
2026-03-08 22:03:51 +0900

    [refactor] 내 정보 조회 로직에서 currentuser 커스텀 어노테이션 사용으로 변경

--- src/main/java/com/bready/server/user/controller/UserController.java
+++ src/main/java/com/bready/server/user/controller/UserController.java
@@ -1,12 +1,10 @@
 package com.bready.server.user.controller;
 
 import com.bready.server.global.auth.CurrentUser;
-import com.bready.server.global.exception.ApplicationException;
 import com.bready.server.global.response.CommonResponse;
 import com.bready.server.user.dto.UpdateBioRequest;
 import com.bready.server.user.dto.UpdateNicknameRequest;
 import com.bready.server.user.dto.UserProfileDto;
-import com.bready.server.user.exception.UserErrorCase;
 import com.bready.server.user.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -14,8 +12,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -35,13 +31,9 @@ public class UserController {
             @ApiResponse(responseCode = "404", description = "사용자 없음",
                 content = @Content(schema = @Schema(implementation = CommonResponse.class)))
     })
-    public CommonResponse<UserProfileDto> me() {
-
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        if (auth == null || !(auth.getPrincipal() instanceof Long userId)) {
-            throw new ApplicationException(UserErrorCase.AUTH_REQUIRED);
-        }
-
+    public CommonResponse<UserProfileDto> me(
+            @CurrentUser Long userId
+    ) {
         return CommonResponse.success(userService.getMyProfile(userId));
     }
 
