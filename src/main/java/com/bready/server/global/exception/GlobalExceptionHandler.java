e7652daf711bf98a62664661dc8c5276e8e322d5
seungin <tmddls91@inha.edu>
2026-03-17 18:52:02 +0900

    [test] PlaceSearchControllerTest 테스트코드 고도화

--- src/main/java/com/bready/server/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/bready/server/global/exception/GlobalExceptionHandler.java
@@ -1,18 +1,19 @@
 package com.bready.server.global.exception;
 
 import com.bready.server.global.response.CommonResponse;
-import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import com.bready.server.stats.exception.StatsErrorCase;
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.validation.ObjectError;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
@@ -121,12 +122,30 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
-    public CommonResponse<Void> handleTypeMismatch(MethodArgumentTypeMismatchException e) {
+    public CommonResponse<?> handleTypeMismatch(MethodArgumentTypeMismatchException e) {
 
         if ("period".equals(e.getName())) {
             throw ApplicationException.from(StatsErrorCase.INVALID_PERIOD);
         }
 
         throw e;
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseEntity<CommonResponse<?>> handleMissingServletRequestParameterException(
+            MissingServletRequestParameterException e) {
+
+        return ResponseEntity
+                .badRequest()
+                .body(CommonResponse.error(400, "필수 요청 파라미터가 누락되었습니다."));
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<CommonResponse<?>> handleConstraintViolationException(
+            ConstraintViolationException e) {
+
+        return ResponseEntity
+                .badRequest()
+                .body(CommonResponse.error(400, "요청 값이 유효하지 않습니다."));
     }
 }
